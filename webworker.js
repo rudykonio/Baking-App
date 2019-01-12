bc2b1a31ef1c1e1cff08fa250f229ae88895c23b
Rudy <rudykonio@gmail.com>
2019-01-12 10:52:29 +0200

    Revert "Add files via upload"
    This reverts commit b04082e

--- webworker.js
+++
@@ -1,9 +0,0 @@
-console.log('worker file');
-onmessage = event => {
-    const data = event.data;
-    switch (data.namespace || data.event) {
-        case 'test':
-            console.log('worker file got a message from main');
-            postMessage({ event: 'test' });
-    }
-}
